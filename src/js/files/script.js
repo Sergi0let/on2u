190516afb2a71cce741a28610a1b211861e6e2df
Sergij Vashkevych <s.vashkevych@gmail.com>
2023-12-08 20:53:52 +0200

    fix header

--- src/js/files/script.js
+++ src/js/files/script.js
@@ -3,6 +3,8 @@ import { isMobile } from "./functions.js";
 // Підключення списку активних модулів
 import { flsModules } from "./modules.js";
 
+let banner = document.getElementById("remove-banner");
+
 banner.onclick = function () {
   this.parentNode.remove();
 };
